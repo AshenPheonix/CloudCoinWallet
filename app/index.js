e9b2ed188c7248a4a97dc32baef37d03d402c785
AshenPheonix <brendlorend@gmail.com>
2017-04-26 18:53:15 -0700

    separation

--- app/index.js
+++ app/index.js
@@ -9,7 +9,7 @@ function createWindow() {
 
   mainWindow.loadURL(`file://${__dirname}/render/index.html`)
 
-  mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
 
   mainWindow.on('closed', ()=> {
     mainWindow = null
